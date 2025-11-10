a3f6c46db55c323f5c70f6a7237ebfa63715d356
zoekittel <kitzy001@mymail.unisa.edu.au>
2025-11-10 20:25:33 +1100

    - Adjusted perform_health_check() method in Staff class to call method from HealthReport class

--- staff.py
+++ staff.py
@@ -9,7 +9,12 @@ This is my own work as defined by the University's Academic Integrity Policy.
 from abc import ABC, abstractmethod
 import random
 
-class Staff(ABC):
+from animal import Animal, Mammal, Reptile, MarineAnimal, BigCat, Monkey
+from enclosure import Enclosure, Terrarium, Savannah, Australiana, African, Aquarium
+from report import HealthReport, BigCatReport
+
+
+class Staff:
 
     def __init__(self, name, id):
         self.__name = name
@@ -17,11 +22,12 @@ class Staff(ABC):
 
     def __str__(self):
         return(
-            f"Staff name: {self.__name}\n"
+            f"Staff name: {self.name}\n"
             f"ID: {self.__id}\n"
         )
 
-    def get_name(self):
+    @property
+    def name(self):
         return self.__name
 
     def get_id(self):
@@ -29,11 +35,41 @@ class Staff(ABC):
 
 
 class Veterinarian(Staff):
-    pass
+
+    def __init__(self, name, id, specialisation=None):
+        super().__init__(name, id)
+        self.__specialisation = specialisation
+
+    def get_specialisation(self):
+        return self.__specialisation
+
+    def set_specialisation(self, specialisation):    # Set specialisation e.g. Mammal Veterinarian.
+        if isinstance(specialisation, str):
+            self.__specialisation = specialisation
+        else:
+            raise TypeError("Specialisation must be a string")
+
+    def perform_health_check(self, animal):
+        if isinstance (animal, Animal):
+            print(f"{self.name} printing report...\n")
+            if isinstance(animal, BigCat):
+                print(BigCatReport.print_report(animal))
+            else:
+                print(HealthReport.print_report(animal))
+
 
 class Zookeeper(Staff):
-    pass
+
+    def __init__(self, name, id):
+        super().__init__(name, id)
+
+    def clean_enclosure(self, enclosure):
+        if isinstance(enclosure, Enclosure):
+            enclosure.clean_enclosure()
+
 
 class Administrator(Staff):
-    pass
+
+    def __init__(self, name, id):
+        super().__init__(name, id)
 
